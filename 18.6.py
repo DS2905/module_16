218fbf0dc5f9e29e67bba3ba2aa6c6211fda9061
Daria <101130753+DS2905@users.noreply.github.com>
2022-05-31 20:35:27 +0500

    Update 18.6.py

--- 18.6.py
+++ 18.6.py
@@ -35,14 +35,28 @@ def convert(message: telebot.types.Message):
     values = message.text.split(' ')
 
     if len(values) > 3:
-        raise ConvertionException('Слишком много параметров')
+        raise ConvertionException('Слишком много параметров.')
 
     quote, base, amount = values
 
     if quote == base:
-        raise ConvertionException(f'Невозможно переветси одинаковые валюты {base}')
+        raise ConvertionException(f'Невозможно переветси одинаковые валюты {base}.')
 
-    quote_ticker, base_ticker = keys[quote], keys[base]
+    try:
+        quote_ticker = keys[quote]
+    except KeyError:
+        raise ConvertionException(f'Не удалось обработать валюту {quote}.')
+
+    try:
+        base_ticker = keys[base]
+    except KeyError:
+        raise ConvertionException(f'Не удалось обработать валюту {base}.')
+
+    try:
+       amount = float(amount)
+    except ValueError:
+        raise ConvertionException(f'Не удалось обработать количество {amount}.')
+
     r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
     total_base = json.loads(r.content)[keys[base]]
     text = f'Цена {amount} {quote} в {base} - {total_base}'
